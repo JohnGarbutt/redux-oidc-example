8ed51345a23d48005aa50a2a8e709a9b465e1a28
John Garbutt <john@johngarbutt.com>
2018-05-10 16:39:00 +0100

    Move to use EGI AAI

--- src/utils/userManager.js
+++ src/utils/userManager.js
@@ -1,15 +1,14 @@
 import { createUserManager } from 'redux-oidc';
 
 const userManagerConfig = {
-  client_id: '581912277515-8pqeloei552og7pa13iufb57iug8vu9k.apps.googleusercontent.com',
+  title: 'Test client',
+  authority: 'https://aai-dev.egi.eu/oidc',
+  client_id: 'b224ccac-9beb-41ce-a702-4b7f1cc5eefc',
   redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback`,
   response_type: 'token id_token',
-  scope: 'openid profile https://www.googleapis.com/auth/youtube.readonly',
-  authority: 'https://accounts.google.com',
-  silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
-  automaticSilentRenew: true,
-  filterProtocolClaims: true,
-  loadUserInfo: true,
+  scope: 'openid profile',
+  filterProtocolClaims: false,
+  debug: true
 };
 
 const userManager = createUserManager(userManagerConfig);
